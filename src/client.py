cd3d177cea32be4815f2548633bdeb4d3bcf881c
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-05-02 11:54:56 +0200

    Rem custom class create line_item

--- src/client.py
+++ src/client.py
@@ -359,30 +359,6 @@ class CreateDeal(HubSpotClient):
         self.make_batch_request(inputs)
 
 
-class CreateLineItem(HubSpotClient):
-    """Creates line items"""
-
-    @batched()
-    def process_requests(self, data_reader):
-        optional_cols = "association_id", "association_category", "association_type_id"
-        inputs = []
-
-        for row in data_reader:
-            if not set(row.keys()).issubset(optional_cols):
-                logging.warning(f"The association is not set, it will be write without, if you need, please add "
-                                f"[association_id, association_category, association_type_id] columns. {row}")
-            else:
-                associations = [{
-                    'to': {'id': str(row.pop('association_id'))},
-                    'types': [{
-                        'associationCategory': row.pop('association_category'),
-                        'associationTypeId': row.pop('association_type_id')
-                    }]
-                }]
-                inputs.append({"associations": associations, "properties": row})
-        self.make_batch_request(inputs)
-
-
 class CreateAssociatedObject(HubSpotClient):
     """Parent class to CRM objects with association - creates objects"""
 
@@ -711,7 +687,6 @@ class AssociationCreate(HubSpotClient):
         inputs = [{k: str(v) for k, v in row.items()} for row in data_reader]
 
         for line in inputs:
-
             endpoint_path = ENDPOINT_MAPPING[self.endpoint]['endpoint'].format(
                 from_object_type=line.get("from_object_type"),
                 to_object_type=line.get("to_object_type"))
@@ -728,7 +703,6 @@ class AssociationRemove(HubSpotClient):
         inputs = [{k: str(v) for k, v in row.items()} for row in data_reader]
 
         for line in inputs:
-
             endpoint_path = ENDPOINT_MAPPING[self.endpoint]['endpoint'].format(
                 from_object_type=line.get("from_object_type"),
                 to_object_type=line.get("to_object_type"))
